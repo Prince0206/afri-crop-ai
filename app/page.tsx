4e8cac3fa5982ed47b7170d2e104517d95e01064
Prince <princewireko@ymail.com>
2026-05-02 14:59:24 +0000

    ResultCard's props require results and onReset

--- app/page.tsx
+++ app/page.tsx
@@ -108,7 +108,7 @@ export default function Home() {
           </p>
         )}
 
-        {result && <ResultCard {...result} />}
+        {result && <ResultCard results={result} onReset={handleClear} />}
 
         <HistoryPanel key={historyKey} />
       </div>
